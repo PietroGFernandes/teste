d953f6220fbcbca975048191e0ba22297b1a0025
PietroGFernandes <70176373+PietroGFernandes@users.noreply.github.com>
2023-05-18 21:49:15 -0300

    Add files via upload

---
+++ src/components/Description/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Title, SubTitle} from "./styles";
+
+function Description(){
+    return(
+        <Container>
+            <Title>Violino</Title>
+            <SubTitle> Tocar é um arte</SubTitle>
+            <SubTitle> 1000R$ </SubTitle>
+           
+        </Container>
+    );
+}
+
+export default Description;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 40px;
+`;
+
+export const Logo = styled.img`
+    width: 300px;
+     height: 300px;
+`;
+
+export const ContainerIcons = styled.div`    
+display: flex;
+align-items: center;
+justify-content: space-between;
+width: 100px;
+`;
+
+
